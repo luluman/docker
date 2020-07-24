6a7bdd31d14c87a578137ca0526031ac898325e0
Matt.Lu <lu.man@outlook.com>
2020-07-24 11:00:35 +0800

    add /data volume

--- work.sh
+++ work.sh
@@ -7,7 +7,7 @@ function work-macos()
          --detach-keys "ctrl-^,ctrl-@" \
          --volume="${home}:/home/root":delegated \
          --volume="${workspace}:/workspace":cached \
-	     --add-host=gerrit.ai.bitmaincorp.vip:10.128.0.97 \
+         --add-host=gerrit.ai.bitmaincorp.vip:10.128.0.97 \
          work/man.lu:1.0 /bin/bash
 }
 
@@ -17,15 +17,17 @@ function work-linux()
   local GID=$(id -g)
   local home=$(realpath ~/.docker/home)
   local workspace=$(realpath ~/workspace)
-  docker run --rm -it \
+  local data=$(realpath /data)
+  docker run -it \
          --name ${USER}-work \
          --user $UID:$GID \
          --detach-keys "ctrl-^,ctrl-@" \
          --volume="${home}:/home/$USER":delegated \
          --volume="${workspace}:/workspace":cached \
+         --volume="${data}:/data":cached \
          --volume="/etc/group:/etc/group:ro" \
          --volume="/etc/passwd:/etc/passwd:ro" \
          --volume="/etc/shadow:/etc/shadow:ro" \
-	     --add-host=gerrit.ai.bitmaincorp.vip:10.128.0.97 \
+         --add-host=gerrit.ai.bitmaincorp.vip:10.128.0.97 \
          work/man.lu:1.0 /bin/bash
 }
