1145941dcbbd6d8c8fc78ee44374096e5e239434
man.lu <man.lu@bitmain.com>
2020-07-30 13:44:02 +0800

    add proxy server to build image

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ docker build -t emacs/build:18.04 -f ./emacs-build.18.04.Dockerfile .
 docker build -t expt/man.lu:0.1 -f ./experiment.Dockerfile .
 docker build -t work/man.lu:0.1 -f ./work.Dockerfile .
 docker build -t work/man.lu:1.0 -f ./work-all.Dockerfile .
+--build-arg http_proxy=http://172.16.74.88:3128 --build-arg https_proxy=http://172.16.74.88:3128
