bd06ed645fee197c82d6229a06522ade776fecd2
man.lu <man.lu@bitmain.com>
2020-08-19 11:15:18 +0800

    add host resolver for work

---
+++ dns_work.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo echo '10.128.0.139 jira.bitmaincorp.vip' >> /etc/hosts
+sudo echo '10.128.0.139 wiki.bitmaincorp.vip' >> /etc/hosts
+sudo echo '10.128.0.97 gerrit.ai.bitmaincorp.vip' >> /etc/hosts
+sudo echo '10.128.0.176 jump.bitmaincorp.vip' >> /etc/hosts
