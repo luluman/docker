27d327c40f19df91ed5248344d2dc20e21910fb8
MattLu <lu.man@outlook.com>
2020-09-11 16:40:33 +0800

    docker metrics rename file when saving

--- scripts/docker_metrics.sh
+++ scripts/docker_metrics.sh
@@ -1,6 +1,7 @@
-docker stats --no-stream | tee --append stats.txt;
+timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+docker stats $1  --no-stream | tee --append "docker_stats_${timestamp}.txt";
 sleep 1;
 while true;
- do docker stats --no-stream | tail -n 1 | tee --append stats.txt; 
- sleep 1; 
+do docker stats $1 --no-stream | tail -n 1 | tee --append "docker_stats_${timestamp}.txt";
+   sleep 1;
 done
