bbc2b25770f20a42d5e0a051c206ce3f7233d369
MattLu <lu.man@outlook.com>
2024-06-02 02:31:53 +0800

    config tailscale using host network

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,7 +9,7 @@ exec /usr/sbin/sshd -D -e &
 trap 'kill -TERM $PID' TERM INT
 echo "Starting Tailscale daemon"
 # -state=mem: will logout and remove ephemeral node from network immediately after ending.
-tailscaled --tun=userspace-networking -no-logs-no-support --state=${TAILSCALE_STATE_ARG} &
+tailscaled -no-logs-no-support --state=${TAILSCALE_STATE_ARG} &
 PID=$!
 until tailscale up --authkey="${TAILSCALE_AUTH_KEY}" --hostname="${TAILSCALE_HOSTNAME}"; do
     sleep 0.1
