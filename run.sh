88ffc2ede50bb88f5a400b88b38a3bc847721559
man.lu <lu.man@outlook.com>
2024-06-07 12:02:27 +0800

    use relative home path

--- run.sh
+++ run.sh
@@ -1,3 +1,28 @@
+function work-linux-server()
+{
+  local home=$(realpath ./home-work)
+  local workspace=$(realpath ~/workspace)
+  local bjnfsdata01=$(realpath /bjnfsdata01)
+  local tmp=$(realpath ./tmp)
+  docker run -t \
+         --privileged \
+         --log-driver=none \
+         --hostname=$(hostname) \
+         --name ${USER}-work-server \
+         --detach-keys "ctrl-^,ctrl-@" \
+         --volume="${home}:/home/$USER":delegated \
+         --volume="${workspace}:/workspace":cached \
+         --volume="${bjnfsdata01}:/bjnfsdata01":cached \
+         --volume="${tmp}:/tmp":cached \
+         --volume="/etc/group:/etc/group:ro" \
+         --volume="/etc/passwd:/etc/passwd:ro" \
+         --volume="/etc/shadow:/etc/shadow:ro" \
+         --volume=/var/run/docker.sock:/var/run/docker.sock \
+         --env-file ${home}/.ssh/vpn.cfg \
+         --detach \
+         mattlu/work-dev:latest
+}
+
 function work-macos()
 {
   local home=~/Documents/docker/home
@@ -15,10 +40,10 @@ function work-linux()
 {
   local UID=$(id -u)
   local GID=$(id -g)
-  local home=$(realpath ~/workspace/.docker/home-work)
+  local home=$(realpath ./home-work)
   local workspace=$(realpath ~/workspace)
   local bjnfsdata01=$(realpath /bjnfsdata01)
-  local tmp=$(realpath ~/workspace/.docker/tmp)
+  local tmp=$(realpath ./tmp)
   docker run -t \
          --privileged \
          --log-driver=none \
@@ -35,88 +60,6 @@ function work-linux()
          --volume=/var/run/docker.sock:/var/run/docker.sock \
          --detach \
          mattlu/work-dev:latest /bin/bash
-}
-
-function work-linux-server()
-{
-  local home=$(realpath ~/workspace/.docker/home-work)
-  local workspace=$(realpath ~/workspace)
-  local bjnfsdata01=$(realpath /bjnfsdata01)
-  local tmp=$(realpath ~/workspace/.docker/tmp)
-  docker run -t \
-         --privileged \
-         --log-driver=none \
-         --hostname=$(hostname) \
-         --name ${USER}-work-server \
-         --detach-keys "ctrl-^,ctrl-@" \
-         --volume="${home}:/home/$USER":delegated \
-         --volume="${workspace}:/workspace":cached \
-         --volume="${bjnfsdata01}:/bjnfsdata01":cached \
-         --volume="${tmp}:/tmp":cached \
-         --volume="/etc/group:/etc/group:ro" \
-         --volume="/etc/passwd:/etc/passwd:ro" \
-         --volume="/etc/shadow:/etc/shadow:ro" \
-         --volume=/var/run/docker.sock:/var/run/docker.sock \
-         --env-file ~/workspace/.docker/home-work/.ssh/vpn.cfg \
-         --detach \
-         mattlu/work-dev:latest
-}
-
-function explore-linux()
-{
-  local UID=$(id -u)
-  local GID=$(id -g)
-  local home=$(realpath ~/.docker/home-explore)
-  local workspace=$(realpath ~/workspace)
-  local data=$(realpath /data)
-  local share=$(realpath /share)
-  local tmp=$(realpath ~/.docker/tmp)
-  docker run -t \
-         --privileged \
-         --log-driver=none \
-         --name ${USER}-explore \
-         --user $UID:$GID \
-         --detach-keys "ctrl-^,ctrl-@" \
-         --volume="${home}:/home/$USER":delegated \
-         --volume="${workspace}:/workspace":cached \
-         --volume="${bjnfsdata01}:/bjnfsdata01":cached \
-         --volume="${tmp}:/tmp":cached \
-         --volume="/etc/group:/etc/group:ro" \
-         --volume="/etc/passwd:/etc/passwd:ro" \
-         --volume="/etc/shadow:/etc/shadow:ro" \
-         --volume=/var/run/docker.sock:/var/run/docker.sock \
-         --detach \
-         --restart=always \
-         mattlu/explore-dev:latest /bin/bash
-}
-
-function explore-linux-server()
-{
-  local UID=$(id -u)
-  local GID=$(id -g)
-  local home=$(realpath ~/.docker/home-explore)
-  local workspace=$(realpath ~/workspace)
-  local data=$(realpath /data)
-  local share=$(realpath /share)
-  local tmp=$(realpath ~/.docker/tmp)
-  docker run -t \
-         --privileged \
-         --log-driver=none \
-         --name ${USER}-explore-server \
-         --detach-keys "ctrl-^,ctrl-@" \
-         --volume="${home}:/home/$USER":delegated \
-         --volume="${workspace}:/workspace":cached \
-         --volume="${data}:/data":cached \
-         --volume="${share}:/share":cached \
-         --volume="${tmp}:/tmp":cached \
-         --volume="/etc/group:/etc/group:ro" \
-         --volume="/etc/passwd:/etc/passwd:ro" \
-         --volume="/etc/shadow:/etc/shadow:ro" \
-         --volume=/var/run/docker.sock:/var/run/docker.sock \
-         --env-file ~/.docker/home-explore/.ssh/vpn.cfg \
-         --detach \
-         --restart=always \
-         mattlu/explore-dev:latest
 }
 
 function work-linux-attach()
@@ -140,40 +83,6 @@ function work-linux-server-exec()
          --detach-keys "ctrl-^,ctrl-@" \
          ${USER}-work-server /bin/bash
 
-}
-
-function explore-linux-attach()
-{
-  docker attach \
-         ${USER}-explore \
-         --detach-keys "ctrl-^,ctrl-@"
-}
-
-function explore-linux-exec()
-{
-  docker exec -ti \
-         --detach-keys "ctrl-^,ctrl-@" \
-         ${USER}-explore /bin/bash
-}
-
-function work-langtool-server()
-{
-  local ngrams=$(realpath ~/.docker/ngrams)
-  docker run -d \
-         --name langtool-server \
-         --network man.lu-net \
-         --volume="${ngrams}:/ngrams:ro" \
-         --restart=unless-stopped \
-         silviof/docker-languagetool
-}
-
-function plantuml-server()
-{
-  docker run -d \
-         --name plantuml-server \
-         --network man.lu-net \
-         --restart=unless-stopped \
-         plantuml/plantuml-server:jetty
 }
 
 function add-network()
