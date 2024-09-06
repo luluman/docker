6df2ab03ed766cad3dd45267b19af01c3e88864f
man.lu <lu.man@outlook.com>
2024-09-07 01:03:15 +0800

    allow some RFC1918 subnets

--- scripts/exit-node.sh
+++ scripts/exit-node.sh
@@ -4,7 +4,12 @@ echo "Starting Tailscale daemon"
 # -state=mem: will logout and remove ephemeral node from network immediately after ending.
 tailscaled --tun=userspace-networking -no-logs-no-support --state=${TAILSCALE_STATE_ARG} &
 PID=$!
-until tailscale up --authkey="${TAILSCALE_AUTH_KEY}" --hostname="${TAILSCALE_HOSTNAME}" --advertise-exit-node; do
+# https://github.com/tailscale/tailscale/issues/5412
+until tailscale up \
+    --authkey="${TAILSCALE_AUTH_KEY}" \
+    --hostname="${TAILSCALE_HOSTNAME}" \
+    --advertise-exit-node \
+    --advertise-routes=10.0.0.0/8,172.16.0.0/12; do
     sleep 0.1
 done
 tailscale status
