a3e645f6e5bf9f929d9b73f72e17b11b4e1afc0d
man.lu <lu.man@outlook.com>
2024-10-28 01:10:48 +0800

    using the default share directory

--- run.sh
+++ run.sh
@@ -3,11 +3,28 @@ function work-linux-server() {
     # cd ~/
     # ln -s your/original/.docker/path .docker
     # ln -s your/original/workspace/path workspace
+
     local home=$(realpath ~/.docker/home-work)
     local workspace=$(realpath ~/workspace)
-    local share=$(realpath ~/share)
     local tmp=$(realpath ~/.docker/tmp)
     local opt=$(realpath ~/.docker/opt)
+
+    local dirs=("/develop01" "/data01")
+    local share=""
+
+    for dir in "${dirs[@]}"; do
+        if [ -d "$dir" ]; then
+            share="$dir"
+            echo "Directory $dir exists. Binding to container and starting Docker."
+            break
+        fi
+    done
+
+    if [ -z "$share" ]; then
+        echo "No shared directory found. Exiting."
+        return 1
+    fi
+
     docker run -t \
         --privileged \
         --log-driver=none \
@@ -18,7 +35,7 @@ function work-linux-server() {
         --volume="${workspace}:/workspace":cached \
         --volume="${tmp}:/tmp":cached \
         --volume="${opt}:/opt":cached \
-        --volume="${share}:/share:ro" \
+        --volume="${share}:${share}:ro" \
         --volume="/etc/group:/etc/group:ro" \
         --volume="/etc/passwd:/etc/passwd:ro" \
         --volume="/etc/shadow:/etc/shadow:ro" \
