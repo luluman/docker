da826b451327dc901c8035dd0c93d35bcbcc73b3
lu.man <lu.man@outlook.com>
2025-05-16 16:53:47 +0800

    refine start script

--- run.sh
+++ run.sh
@@ -4,50 +4,42 @@ function work-linux-server() {
     # ln -s your/original/.docker/path .docker
     # ln -s your/original/workspace/path workspace
 
-    local home=$(realpath ~/.docker/home-work)
-    local workspace=$(realpath ~/workspace)
-    local tmp=$(realpath ~/.docker/tmp)
-    local opt=$(realpath ~/.docker/opt)
+    local base="$HOME/.docker"
+    declare -a volumes=(
+        --volume="$(realpath "$HOME/workspace"):/workspace:cached"
+        --volume="$(realpath "$base/home-work"):$HOME:delegated"
+        --volume="$(realpath "$base/tmp"):/tmp:cached"
+        --volume="$(realpath "$base/opt"):/opt:cached"
+        --volume="/var/run/docker.sock:/var/run/docker.sock"
+    )
 
-    local share_data=$(realpath /share_data)
-    local software_data=$(realpath /software_data)
+    # List your shared dirs here (expand as needed)
+    declare -a shared_dirs=(
+        "/share_data"
+        "/software_data"
+    )
+    for dir in "${shared_dirs[@]}"; do
+        if [ -d "$dir" ]; then
+            volumes+=(--volume="$(realpath "$dir"):$dir:ro")
+        fi
+    done
 
-    getent passwd &> ~/.docker/etc/passwd
-    getent group &> ~/.docker/etc/group
-
-    local group_file=$(realpath ~/.docker/etc/group)
-    local passwd_file=$(realpath ~/.docker/etc/passwd)
-
-    local container_name=${USER}-work-server
+    mkdir -p "$base/etc"
+    getent passwd > "$base/etc/passwd"
+    getent group > "$base/etc/group"
+    volumes+=(--volume="$base/etc/passwd:/etc/passwd:ro" --volume="$base/etc/group:/etc/group:ro")
 
     docker run -t \
         --privileged \
         --log-driver=none \
-        --hostname=$(hostname) \
-        --name ${container_name} \
+        --hostname="$(hostname)" \
+        --name "${USER}-work-server" \
         --detach-keys "ctrl-^,ctrl-@" \
-        --volume="${home}:${HOME}":delegated \
-        --volume="${workspace}:/workspace":cached \
-        --volume="${tmp}:/tmp":cached \
-        --volume="${opt}:/opt":cached \
-        --volume="${share_data}":/share_data:ro \
-        --volume="${software_data}":/software_data:ro \
-        --volume="{group_file}:/etc/group:ro" \
-        --volume="{passwd_file}:/etc/passwd:ro" \
-        --volume=/var/run/docker.sock:/var/run/docker.sock \
-        --env-file ${home}/.ssh/vpn.cfg \
-        --restart=always \
-        --detach \
+        "${volumes[@]}" \
+        --env-file "$base/home-work/.ssh/vpn.cfg" \
+        --restart=always --detach \
         mattlu/work-dev:latest
 
-    # copy driver
-    local latest_drv=$(find /usr/local -type d -name 'houmo_drv*' -not -empty | sort -r | head -n 1)
-    if [[ "${latest_drv}" != *houmo_drv* ]]; then
-        echo -e "\e[31mcannot find driver in /usr/local\e[0m"
-    else
-        echo -e "\e[31musing latest driver ${latest_drv}\e[0m"
-        docker cp ${latest_drv} ${container_name}:/usr/local/houmo-sdk
-    fi
 }
 
 function work-linux-server-exec() {
